f43231a3361bc1dadb851a7f0271ad7fbc41b99d
Mitch Haddadi <12067255+dfirsec@users.noreply.github.com>
2022-10-30 10:21:35 -0400

    Minor Update
    Changed 'link_parser' results to a set to remove duplicate entries from results.

--- web_dl_by_type.py
+++ web_dl_by_type.py
@@ -3,6 +3,7 @@ import os
 import platform
 import sys
 from pathlib import Path
+from typing import Iterable
 from urllib.parse import urljoin
 
 import aiofiles
@@ -13,20 +14,20 @@ import validators
 from bs4 import BeautifulSoup
 
 __author__ = "DFIRSec (@pulsecode)"
-__version__ = "0.1"
+__version__ = "0.1.0"
 __description__ = "Download web hosted files by filetype/file extension."
 
 # Creates "Downloads" directory in the same directory as the script.
-root = Path(__file__).parent.resolve()
-filepath = root.joinpath("Downloads")
-filepath.mkdir(parents=True, exist_ok=True)
+ROOT = Path(__file__).parent.resolve()
+FILEPATH = ROOT.joinpath("Downloads")
+FILEPATH.mkdir(parents=True, exist_ok=True)
 
-headers = {
+_headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
 }
 
 
-def link_parser(req_url, filetype):
+def link_parser(req_url: str, filetype: str) -> Iterable:
     """
     Takes a URL and a filetype as arguments, and returns a generator object that yields all the links
     on the page that end with the specified filetype
@@ -35,13 +36,13 @@ def link_parser(req_url, filetype):
     :param filetype: The filetype you want to search for
     """
     try:
-        response = requests.get(req_url, headers=headers)
+        response = requests.get(req_url, headers=_headers, timeout=10)
         response.raise_for_status()
     except (ConnectionError, requests.exceptions.HTTPError) as error:
         sys.exit(f"[x] {error}")
     else:
-        soup = BeautifulSoup(response.text, "lxml")
-        if results := [link.get("href") for link in soup.find_all("a") if link.get("href") is not None]:
+        soup = BeautifulSoup(response.text, "html.parser")
+        if results := {link.get("href") for link in soup.find_all("a") if link.get("href") is not None}:
             for link in results:
                 if os.path.basename(link).split(".")[-1] == filetype:
                     yield urljoin(req_url, link) if req_url not in link else link
@@ -56,9 +57,9 @@ async def downloader(session, download_url):
     :return: The response object is being returned.
     """
     async with async_timeout.timeout(10):
-        async with session.get(download_url, headers=headers) as response:
+        async with session.get(download_url, headers=_headers) as response:
             filename = os.path.basename(download_url)
-            dlpath = Path(filepath / filename)
+            dlpath = Path(FILEPATH / filename)
             if not dlpath.resolve().exists():
                 print(f"[+] Downloading: {filename}")
                 async with aiofiles.open(dlpath, "wb") as fileobj:
@@ -72,7 +73,7 @@ async def downloader(session, download_url):
             print(f"[-] File already exists: {filename}")
 
 
-async def main(url, filetype):
+async def main(url: str, filetype: str):
     """
     Takes a URL and a file type as arguments, and downloads all files of that type from the URL
 
@@ -91,23 +92,23 @@ async def main(url, filetype):
 if __name__ == "__main__":
 
     # Check python version.
-    if sys.version_info.minor < 7:
-        print(f"[!] Tested on Python version 3.7+. May not work with {platform.python_version()}\n")
+    if sys.version_info.minor < 8:
+        print(f"[!] Tested on Python version 3.8+. May not work with {platform.python_version()}\n")
 
     # Check if the user has entered the correct number of arguments.
     if len(sys.argv) < 3:
         sys.exit(f"Usage: python {os.path.basename(__file__)} <URL> <FILE TYPE>")
     else:
-        mainurl = sys.argv[1]
-        ftype = sys.argv[2]
+        MAINURL = sys.argv[1]
+        FILETYPE = sys.argv[2]
 
     # Check if the URL is valid.
-    if not validators.url(mainurl):  # type: ignore
-        sys.exit(f"[!] {mainurl} is not a valid URL")
+    if not validators.url(MAINURL):  # type: ignore
+        sys.exit(f"[!] {MAINURL} is not a valid URL")
 
     # Resolves issue: https://github.com/encode/httpx/issues/914
     if platform.system() == "Windows":
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
     # Running the main function asynchronously.
-    asyncio.run(main(mainurl, ftype))
+    asyncio.run(main(MAINURL, FILETYPE))
