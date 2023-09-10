97e2a2ed3ebea3e2c23ed76e15fda8281133947d
Mitch Haddadi <12067255+dfirsec@users.noreply.github.com>
2023-09-10 07:05:05 -0400

    Updated
    Included connection retry and wait.
    Refactoring and corrections.

--- web_dl_by_type.py
+++ web_dl_by_type.py
@@ -14,24 +14,46 @@ import aiofiles
 import aiohttp
 import async_timeout
 from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
 from rich.console import Console
 
 console = Console()
 
 author = "DFIRSec (@pulsecode)"
-version = "0.1.2"
+version = "0.1.3"
 
 
-class FileDownloader(object):
-    """Contains main program functionality."""
+class CustomClientError(aiohttp.ClientError):
+    """Custom ClientError exception."""
 
     def __init__(self):
-        """Initialize the class."""
-        self.console = Console()
+        """Initialize class."""
+        super().__init__("Possible anti-scraping measure detected")
+
+
+class FileDownloader:
+    """Contains main program functionality."""
+
+    def __init__(self) -> None:
+        """Initialize the class instance.
+
+        Attributes:
+        - `ue` : Instance of `UserAgent` from `fake_useragent` module.
+        - `root` : Resolved path to the directory containing the current file.
+        - `filepath` : Path to the "Downloads" directory located within the root directory.
+                    If this directory doesn't exist, it's created.
+        - `headers` : A dictionary containing request headers, including a randomly generated
+                    User-Agent, Accept types, and Accept-Language specifications.
+        """
+        self.ua = UserAgent()
         self.root = Path(__file__).parent.resolve()
         self.filepath = self.root.joinpath("Downloads")
         self.filepath.mkdir(parents=True, exist_ok=True)
-        self.headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0"}
+        self.headers = {
+            "User-Agent": self.ua.random,
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Accept-Language": "en-US,en;q=0.5",
+        }
 
         logging.basicConfig(
             level=logging.ERROR,
@@ -39,14 +61,45 @@ class FileDownloader(object):
             handlers=[logging.FileHandler("error_log.log")],
         )
 
-    async def get_page(self, session: aiohttp.ClientSession, req_url: str) -> str:
+    async def get_page(self, session: aiohttp.ClientSession, req_url: str) -> str | None:
         """Get the HTML page."""
-        async with session.get(req_url, headers=self.headers, timeout=aiohttp.ClientTimeout(total=10)) as response:
-            response.raise_for_status()
-        return await response.text()
+        max_retries = 3
+
+        async def raise_client_error() -> None:
+            """Raise a ClientError exception.
+
+            Raises:
+                CustomClientError: Custom ClientError exception.
+            """
+            raise CustomClientError()
+
+        for retry in range(max_retries):
+            try:
+                async with session.get(
+                    req_url,
+                    headers=self.headers,
+                    timeout=aiohttp.ClientTimeout(total=10),
+                ) as response:
+                    response.raise_for_status()
+
+                    # Check if status is 403 Forbidden or the content length is too small
+                    if response.status == 403 or len(await response.text()) < 100:
+                        console.print("[red][!] The server might be blocking our requests.")
+                        await raise_client_error()
+
+                    return await response.text()
+
+            except (aiohttp.ClientError, asyncio.TimeoutError):
+                if retry == max_retries - 1:
+                    await raise_client_error()
+        return None
 
     async def link_parser(self, base_url: str, filetype: str, html: str) -> AsyncIterator[str]:
-        """Parse the HTML page for links."""
+        """Parse the HTML page for links.
+
+        Yields:
+            str: The URL of the file.
+        """
         pattern = re.compile(r"https?://\S+" + re.escape(filetype))
         for match in pattern.finditer(html):
             yield match.group()
@@ -65,7 +118,7 @@ class FileDownloader(object):
             if final_src is not None:
                 url = urljoin(base_url, final_src)
                 path = urlparse(url).path
-                _, ext = Path(path).suffix
+                ext = Path(path).suffix
                 ext = ext.split("?")[0]
 
                 if ext.lower() == filetype.lower():
@@ -82,7 +135,7 @@ class FileDownloader(object):
         """Download the file."""
         async with semaphore:
             path = urlparse(download_url).path
-            _, ext = Path(path).suffix
+            ext = Path(path).suffix
             filename = Path(path).name
             dlpath = Path(self.filepath / filename)
 
@@ -93,7 +146,7 @@ class FileDownloader(object):
             try:
                 async with async_timeout.timeout(10), session.get(download_url, headers=self.headers) as response:
                     if ext and ext.lower().split(".")[1] == filetype.lower() and not dlpath.resolve().exists():
-                        self.console.print(f"[green][+] Downloading: {filename}")
+                        console.print(f"[green][+] Downloading: {filename}")
                         async with aiofiles.open(dlpath, "wb") as fileobj:
                             while True:
                                 chunk = await response.content.read(1024)
@@ -103,12 +156,12 @@ class FileDownloader(object):
                         await response.release()
 
             except asyncio.TimeoutError:
-                self.console.print(f"[red][!] Timeout error when downloading '{download_url}'.")
+                console.print(f"[red][!] Timeout error when downloading '{download_url}'.")
                 logging.exception(f"Timeout error when downloading '{download_url}'.")
                 failed_downloads.append(download_url)
 
             except Exception as exc:
-                self.console.print(f"[red][!] Error downloading '{download_url}': {type(exc).__name__}: {exc}")
+                console.print(f"[red][!] Error downloading '{download_url}': {type(exc).__name__}: {exc}")
                 logging.exception(f"Error downloading '{download_url}': {type(exc).__name__}")
 
     async def main(self, url: str, filetype: str) -> None:
@@ -122,14 +175,20 @@ class FileDownloader(object):
             semaphore = asyncio.Semaphore(max_concurrent_downloads)
             failed_downloads = []
 
-            async for urlitem in self.link_parser(url, filetype, html):
-                task = asyncio.create_task(self.downloader(semaphore, session, urlitem, filetype, failed_downloads))
-                tasks.append(task)
+            # Check if the HTML page was retrieved successfully.
+            if html is not None:
+                async for urlitem in self.link_parser(url, filetype, html):
+                    task = asyncio.create_task(self.downloader(semaphore, session, urlitem, filetype, failed_downloads))
+                    tasks.append(task)
+            else:
+                console.print(f"[red][!] Error retrieving HTML page from '{url}'")
+                logging.error(f"Error retrieving HTML page from '{url}'")
 
+            # Download the files.
             await asyncio.gather(*tasks)
 
             if failed_downloads:
-                self.console.print("\n[gold1][!] Retrying failed downloads...")
+                console.print("\n[gold1][!] Retrying failed downloads...")
                 tasks = []
 
                 for failed in failed_downloads:
@@ -150,8 +209,8 @@ def is_valid_url(url: str) -> bool:
 
 if __name__ == "__main__":
     # Check python version.
-    if sys.version_info < (3, 8):
-        console.print(f"[plum4]Tested on Python v3.8+. May not work properly with v{platform.python_version()}\n")
+    if sys.version_info < (3, 9):
+        console.print(f"[plum4]Tested on Python v3.9+. May not work properly with v{platform.python_version()}\n")
 
     # Check if the user has entered the correct number of arguments.
     MAINURL = ""
